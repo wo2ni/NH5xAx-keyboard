1ac8094822a3109e76dfa3b64ca38dca780165fd
wo2ni <i_o_i_o@163.com>
2024-01-18 18:36:02 +0800

    :rocket: update

--- keyboard_led.sh
+++ keyboard_led.sh
@@ -162,7 +162,7 @@ case $1 in
     *)  #使用帮助;
         clear
         printf "\t${Red}#++++++++++++++++++++#${Cls}\n\a"
-        printf "\t${Yellow}#装B专属,炫酷键盘灯效;${Cls}\n"
+        printf "\t${Yellow}#NH5xAx,炫酷键盘灯效;${Cls}\n"
         printf "\t${Red}#++++++++++++++++++++#${Cls}\n"
         printf "装B神器,共计<--${Red}6${Cls}-->${Green}种模式,待我细细到来;${Cls}\n"
         printf "<--${Green}0${Cls}-->${Blue}闪烁模式,一直闪啊闪的;${Cls}\n"
